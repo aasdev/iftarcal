230d6ed4356f80c781bc3bc4a6ab7e7c978eb140
Anees Shaikh <aasdevaddr@gmail.com>
2014-07-01 01:28:50 -0400

    Added new columns for iftar coordinators and volunteers to support signup for those roles.

--- iftarcal.sql
+++ iftarcal.sql
@@ -1,4 +1,8 @@
 create table iftarschedule (
 `date` date NOT NULL PRIMARY KEY,
 numhosts SMALLINT(1) NOT NULL default 0,
-hosts TEXT);
+hosts TEXT,
+numcoord SMALLINT(1) NOT NULL default 0,
+coordinators TEXT,
+numvolun SMALLINT(1) NOT NULL default 0,
+volunteers TEXT);
